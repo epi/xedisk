a927abf0e9ff73119593e2d5b7a648f013866232
epi <epi@atari8.info>
2010-08-03 02:53:25 +0200

    [xedisk] verbose-aware dir, initial impl for extract (doesn't work with subdirs), raii junk for opening both fs and img

--- xedisk.d
+++ xedisk.d
@@ -25,6 +25,7 @@ import std.string;
 import std.conv;
 import std.getopt;
 import std.date;
+import std.contracts;
 
 import image;
 import filesystem;
@@ -34,44 +35,107 @@ uint totalSectors = 720;
 bool verbose;
 string imageFileName;
 
+struct OpenFileSystem
+{
+	this(string filename, bool readOnly = true, bool throwOnUnknownFilesystem = true)
+	{
+		image = Image.open(filename, readOnly);
+		scope (failure) image.close();
+		auto e = collectException(fs = FileSystem.open(image));
+		if (e !is null && throwOnUnknownFilesystem)
+			throw e;
+	}
+
+	~this()
+	{
+		if (fs !is null)
+			fs.close();
+		image.close();
+	}
+
+	Image image;
+	FileSystem fs;
+}
+
 void info(string[] args)
 {
-	auto image = Image.open(imageFileName, true);
-	scope (exit) image.close();
-	writeln("Image file name:   ", imageFileName);
-	writeln("Total sectors:     ", image.totalSectors);
-	writeln("Bytes per sectors: ", image.bytesPerSector);
+	auto ofs = OpenFileSystem(imageFileName, true, false);
 
-	auto fs = FileSystem.open(image);
-	scope (exit) fs.close();
-	writeln("\nFilesystem:        ", fs.name);
-	writeln("Label:             ", fs.getLabel());
-	writeln("Free sectors:      ", fs.getFreeSectors());
+	writeln("Image file name:   ", imageFileName);
+	writeln("Total sectors:     ", ofs.image.totalSectors);
+	writeln("Bytes per sectors: ", ofs.image.bytesPerSector);
+
+	writeln("\nFilesystem:        ", ofs.fs is null ? "n/a" : ofs.fs.name);
+	writeln("Label:             ", ofs.fs is null ? "n/a" : ofs.fs.getLabel());
+	writeln("Free sectors:      ", ofs.fs is null ? "n/a" : to!string(ofs.fs.getFreeSectors()));
 }
 
 void dir(string[] args)
 {
-	auto image = Image.open(imageFileName, true);
-	scope (exit) image.close();
-	auto fs = FileSystem.open(image);
-	scope (exit) fs.close();
-	
-	fs.listDir((const ref FileInfo dirEntry)
+	auto ofs = OpenFileSystem(imageFileName);
+	if (verbose)
 	{
-		writefln("%c%c%c%c %-12s %8d",
-			dirEntry.isDeleted ? "x" : " ",
-			dirEntry.isNotClosed ? "u" : " ",
-			dirEntry.isDirectory ? ":" : " ",
-			dirEntry.isReadOnly ? "*" : " ",
-			dirEntry.name,
-			dirEntry.length);
+		uint files = 0;
+		writeln("Filesystem: ", ofs.fs.name);
+		writeln("Label:      ", ofs.fs.getLabel(), "\n");
+		ofs.fs.listDir((const ref FileInfo dirEntry)
+		{
+			writefln("%c%c%c%c %-12s %8d",
+				dirEntry.isDeleted ? "x" : " ",
+				dirEntry.isNotClosed ? "u" : " ",
+				dirEntry.isDirectory ? ":" : " ",
+				dirEntry.isReadOnly ? "*" : " ",
+				dirEntry.name,
+				dirEntry.length);
+			++files;
+			return true;
+		}, args.length > 3 ? args[3] : "/*.*");
+		writefln("\n%11d files", files);
+		writefln("%5d/%5d sectors free", ofs.fs.getFreeSectors(), ofs.image.totalSectors);
+	}
+	else
+	{
+		ofs.fs.listDir((const ref FileInfo dirEntry)
+		{
+			writefln(dirEntry.name);
+			return true;
+		}, args.length > 3 ? args[3] : "/*.*");
+	}
+}
+
+void extract(string[] args)
+{
+	auto ofs = OpenFileSystem(imageFileName);
+	auto fs = ofs.fs;
+
+	bool extractOne(const ref FileInfo dirEntry)
+	{
+		if (verbose)
+			writeln(dirEntry.name);
+		if (dirEntry.isDirectory || dirEntry.isDeleted || dirEntry.isNotClosed)
+		{
+			throw new Exception("Not implemented");
+		}
+		auto ifile = fs.openFile(dirEntry.name, "rb");
+		scope (exit) ifile.close();
+		auto ofile = File(dirEntry.name, "wb");
+
+		auto blk = new ubyte[4096];
+		
+		for (size_t len; (len = ifile.read(blk)) > 0; )
+			ofile.rawWrite(blk[0 .. len]);
 		return true;
-	});
+	}
+
+	foreach (fname; args[3 .. $])
+	{
+		fs.listDir(&extractOne, fname);
+	}
 }
 
 void printHelp(string[] args)
 {
-	debug {} else write(
+	write(
 		"Atari XL/XE Disk Image Utility\n" ~
 		"\nUsage:\n",
 		args[0], " command disk_image_file [options]\n" ~
@@ -110,6 +174,7 @@ int main(string[] args)
 			"help":&printHelp,
 			"info":&info,
 			"dir":&dir,
+			"extract":&extract,
 			];
 		foreach (cmd, fun; funcs)
 		{
